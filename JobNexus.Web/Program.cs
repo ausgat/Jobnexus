4bd311355f775ee4ef1757cae78a7e5617b6d62b
Austin Gatchell <ausgat95@gmail.com>
2026-04-18 11:06:13 -0500

    Add not found page

--- JobNexus.Web/Program.cs
+++ JobNexus.Web/Program.cs
@@ -35,6 +35,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStatusCodePagesWithReExecute("/notfound", createScopeForStatusCodePages: true);
 
 app.UseAntiforgery();
 
