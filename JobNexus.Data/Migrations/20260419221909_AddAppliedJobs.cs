96b7eb1fd986b1584a94eb28c72295bb7976efa3
Austin Gatchell <ausgat95@gmail.com>
2026-04-19 17:19:48 -0500

    Add applied jobs migration

---
+++ JobNexus.Data/Migrations/20260419221909_AddAppliedJobs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobNexus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppliedJobs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
